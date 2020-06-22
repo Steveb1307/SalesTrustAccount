c3b386eda7dc3898d2055ad4466004d2ddc61dfb
Steveb1307 <35835314+Steveb1307@users.noreply.github.com>
2020-06-22 20:03:30 +1000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesTrustAccount.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
